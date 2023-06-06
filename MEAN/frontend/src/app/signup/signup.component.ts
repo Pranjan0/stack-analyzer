9b9d2331bea5a4b946f4a779af1413a112f115e0
m-triple-m <triplem656@gmail.com>
2023-06-06 19:29:20 +0530

    ..

--- MEAN/frontend/src/app/signup/signup.component.ts
+++ MEAN/frontend/src/app/signup/signup.component.ts
@@ -1,20 +1,59 @@
 // signup.component.ts
 
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
-  name = new FormControl();
-  email = new FormControl();
-  password = new FormControl();
+export class SignupComponent implements OnInit {
 
-  onSubmit() {
-    // Handle the form submission logic here
-    console.log(this.name);
+  signupform: any;
+  url = 'http://localhost:5000';
+  constructor(private http: HttpClient,private fb: FormBuilder, private router: Router) {}
+  
+  ngOnInit(): void {
+    this.initform();
   }
+
+  initform() {
+    this.signupform = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.email],
+      password: ['', Validators.required],
+      cpassword: [''],
+    })
+  }
+
+  submitForm(formdata:any) {
+    console.log(formdata);
+    if (this.signupform.invalid) {
+      Swal.fire({
+        icon: 'error',
+        title: 'OOps!!',
+        text: 'Fill form correctly'
+      })
+      return;
+    }
+    this.addUser(formdata).subscribe((res) => {
+      console.log(res);
+      Swal.fire({
+        icon: 'success',
+        title: 'Success!!',
+        text: 'You have successfully registered'
+      }).then(d => {
+        this.router.navigate(['/login']);
+      })
+    })
+  }
+
+  addUser(userdata: any) {
+    return this.http.post(this.url + '/user/add', userdata);
+  }
+
 }
